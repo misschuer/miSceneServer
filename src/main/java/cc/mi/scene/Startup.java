1d88347f33599373b69cf4e3333a4eebf7e647df
gy <misschuer@163.com>
2017-07-14 16:53:44 +0800

    增加场景服的socket链接

---
+++ src/main/java/cc/mi/scene/Startup.java
@@ -0,0 +1,38 @@
+package cc.mi.scene;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.ini4j.Config;
+import org.ini4j.Ini;
+import org.ini4j.Profile.Section;
+
+import cc.mi.scene.sceneClient.SceneClient;
+
+public class Startup {
+	private static final String SCENE_CLIENT = "sceneClient";
+	private static final String IP = "ip";
+	private static final String PORT = "port";
+	
+	private static void loadConfig() throws NumberFormatException, Exception {
+		Config cfg = new Config();
+		URL url = Startup.class.getResource("/config.ini");
+		Ini ini = new Ini();
+        ini.setConfig(cfg);
+        try {
+        	// 加载配置文件  
+        	ini.load(url);
+
+        	Section section = ini.get(SCENE_CLIENT);
+        	SceneClient.start(section.get(IP), Integer.parseInt(section.get(PORT)));
+        	
+        } catch (IOException e) {
+        	e.printStackTrace();
+	    }  
+	}
+
+	public static void main(String[] args) throws NumberFormatException, Exception {
+		loadConfig();
+	}
+
+}
---
+++ src/main/java/cc/mi/scene/sceneClient/SceneClientHandler.java
@@ -0,0 +1,33 @@
+package cc.mi.scene.sceneClient;
+
+import cc.mi.core.coder.Coder;
+import cc.mi.scene.system.SystemManager;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class SceneClientHandler extends SimpleChannelInboundHandler<Coder> {
+	
+	public void channelActive(final ChannelHandlerContext ctx) {
+		System.out.println("connect to center success");
+		SystemManager.setCenterChannel(ctx.channel());
+	}
+	
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		
+	}
+	
+	@Override
+	public void channelRead0(final ChannelHandlerContext ctx, final Coder coder) throws Exception {
+		
+	}
+	
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("gate client inactive");
+		ctx.fireChannelInactive();
+	}
+
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable throwable) {
+		throwable.printStackTrace();
+		ctx.close();
+	}
+}
