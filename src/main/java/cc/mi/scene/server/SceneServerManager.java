f8e8dc007e2be52553e144f0ccf950127e4275bd
gong <403575226@qq.com>
2018-07-02 20:56:40 +0800

    往中心服注册消息

--- src/main/java/cc/mi/scene/server/SceneServerManager.java
+++ src/main/java/cc/mi/scene/server/SceneServerManager.java
@@ -1,10 +1,26 @@
 package cc.mi.scene.server;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import cc.mi.core.constance.IdentityConst;
+import cc.mi.core.handler.Handler;
 import cc.mi.core.manager.ServerManager;
 
 public class SceneServerManager extends ServerManager {
 	private static SceneServerManager instance;
+	// 消息收到以后的回调
+	private static final Map<Integer, Handler> handlers = new HashMap<>();
+	private static final List<Integer> opcodes;
+	
+	static {
+		
+		opcodes = new LinkedList<>();
+		opcodes.addAll(handlers.keySet());
+	}
 	
 	public static SceneServerManager getInstance() {
 		if (instance == null) {
@@ -14,6 +30,6 @@ public class SceneServerManager extends ServerManager {
 	}
 	
 	public SceneServerManager() {
-		super(IdentityConst.SERVER_TYPE_SCENE);
+		super(IdentityConst.SERVER_TYPE_SCENE, opcodes);
 	}
 }
