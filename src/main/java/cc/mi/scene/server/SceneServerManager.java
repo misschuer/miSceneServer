9f3bb2eb007c87e8c58f5915fa69816f83d5afce
Your Name <you@example.com>
2018-12-04 17:16:29 +0800

    抽象方法

--- src/main/java/cc/mi/scene/server/SceneServerManager.java
+++ src/main/java/cc/mi/scene/server/SceneServerManager.java
@@ -5,10 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import cc.mi.core.callback.AbstractCallback;
 import cc.mi.core.callback.Callback;
@@ -16,10 +12,8 @@ import cc.mi.core.constance.IdentityConst;
 import cc.mi.core.constance.ObjectType;
 import cc.mi.core.generate.Opcodes;
 import cc.mi.core.generate.stru.BinlogInfo;
-import cc.mi.core.handler.Handler;
 import cc.mi.core.log.CustomLogger;
 import cc.mi.core.manager.ServerManager;
-import cc.mi.core.packet.Packet;
 import cc.mi.core.utils.ServerProcessBlock;
 import cc.mi.core.utils.TimestampUtils;
 import cc.mi.scene.handler.BinlogDataModifyHandler;
@@ -32,26 +26,19 @@ import cc.mi.scene.info.WaitJoinInfo;
 
 public class SceneServerManager extends ServerManager {
 	static final CustomLogger logger = CustomLogger.getLogger(SceneServerManager.class);
-	
 	private static SceneServerManager instance = new SceneServerManager();
-	// 消息收到以后的回调
-	private static final Map<Integer, Handler> handlers = new HashMap<>();
-	private static final List<Integer> opcodes;
 	
-	// 帧刷新
-	private final ScheduledExecutorService excutor = Executors.newScheduledThreadPool(1);
-	// 消息包队列
-	private final Queue<Packet> packetQueue = new LinkedList<>();
-	// 当前帧刷新执行的代码逻辑
-	protected ServerProcessBlock process;
-	// 最后一次执行帧刷新的时间戳
-	protected long timestamp = 0;
 	// 对象管理
 	private final SceneObjectManager objManager = new SceneObjectManager();
 	
 	private final Map<String, WaitJoinInfo> waitJoinHash = new HashMap<>();
 	
-	static {
+	public static SceneServerManager getInstance() {
+		return instance;
+	}
+	
+	@Override
+	protected void onOpcodeInit() {
 		handlers.put(Opcodes.MSG_BINLOGDATAMODIFY, new BinlogDataModifyHandler());
 		handlers.put(Opcodes.MSG_CREATEMAP, new CreateMapHandler());
 		handlers.put(Opcodes.MSG_JOINMAPMSG, new JoinMapHandler());
@@ -60,35 +47,15 @@ public class SceneServerManager extends ServerManager {
 		handlers.put(Opcodes.MSG_PLAYERLEAVEMAP, new LeaveMapHandler());
 		handlers.put(Opcodes.MSG_DELETEMAP, new DeleteMapHandler());
 		
-		opcodes = new LinkedList<>();
 		opcodes.addAll(handlers.keySet());
-	}	
-	
-	public static SceneServerManager getInstance() {
-		return instance;
 	}
 	
 	public SceneServerManager() {
-		super(IdentityConst.SERVER_TYPE_SCENE, opcodes);
-		excutor.scheduleWithFixedDelay(new Runnable() {
-			@Override
-			public void run() {
-				long prev = instance.timestamp;
-				long now = System.currentTimeMillis();
-				int diff = 0;
-				if (prev > 0) diff = (int) (now - prev);
-				instance.timestamp = now;
-				if (diff < 0 || diff > 1000) {
-					logger.warnLog("too heavy logical that execute");
-				}
-				try {
-					instance.doWork(diff);
-				} catch (Throwable t) {
-					t.printStackTrace();
-				}
-			}
-		}, 1000, 100, TimeUnit.MILLISECONDS);
-		
+		super(IdentityConst.SERVER_TYPE_SCENE);
+	}
+	
+	@Override
+	protected void onProcessInit() {
 		this.process = new ServerProcessBlock() {
 			@Override
 			public void run(int diff) {
@@ -100,7 +67,8 @@ public class SceneServerManager extends ServerManager {
 	/**
 	 * 进行帧刷新
 	 */
-	private void doWork(int diff) {
+	@Override
+	protected void doWork(int diff) {
 		// 初始化服务器
 		this.doProcess(diff);
 		// 处理包信息
@@ -127,7 +95,7 @@ public class SceneServerManager extends ServerManager {
 	
 	public void onBinlogDatasUpdated(List<BinlogInfo> binlogInfoList) {
 		for (BinlogInfo binlogInfo : binlogInfoList) {
-			objManager.parseBinlogInfo(binlogInfo);
+			this.objManager.parseBinlogInfo(binlogInfo);
 		}
 	}
 	
@@ -140,41 +108,9 @@ public class SceneServerManager extends ServerManager {
 		this.startReady();
 	}
 	
-	private void doProcess(int diff) {
-		if (this.process != null) {
-			this.process.run(diff);
-		}
-	}
-	
-	private void dealPacket() {
-		while (!packetQueue.isEmpty()) {
-			Packet packet = packetQueue.poll();
-			this.invokeHandler(packet);
-		}
-	}
-	
-	private void invokeHandler(Packet packet) {
-		int opcode = packet.getOpcode();
-		Handler handle = handlers.get(opcode);
-		if (handle != null) {
-			handle.handle(null, this.centerChannel, packet);
-		}
-	}
-	
-	public void pushPacket(Packet packet) {
-		synchronized (this) {
-			packetQueue.add(packet);
-		}
-	}
-	
 	protected void addTagWatchCallback(String ownerTag, Callback<Void> callback) {
 		objManager.addCreateCallback(ownerTag, callback);
 	}
-
-	public SceneObjectManager getObjManager() {
-		return objManager;
-	}
-	
 	
 	private void doUpdate(int diff) {
 		this.updateWaitJoin();
@@ -229,5 +165,9 @@ public class SceneServerManager extends ServerManager {
 		for (String guid : removeList) {
 			waitJoinHash.remove(guid);
 		}
+	}
+	
+	public SceneObjectManager getObjManager() {
+		return objManager;
 	}
 }
